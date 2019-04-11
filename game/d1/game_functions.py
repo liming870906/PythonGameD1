720d8b1d959acc47c2a11aa58d5cf82ed6fdcaa9
liming870906 <370322364@qq.com>
2019-04-11 13:48:56 +0800

    抽取按下与抬起事件方法

--- game/d1/game_functions.py
+++ game/d1/game_functions.py
@@ -11,23 +11,43 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-            elif event.key == pygame.K_UP:
-                ship.moving_up = True
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-            elif event.key == pygame.K_UP:
-                ship.moving_up = False
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = False
+            check_keyup_events(event, ship)
+
+
+def check_keyup_events(event, ship):
+    """
+    抬起事件
+    :param event:
+    :param ship:
+    :return:
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+
+def check_keydown_events(event, ship):
+    """
+    按下事件
+    :param event:
+    :param ship:
+    :return:
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
 
 
 def update_screen(ai_settings,screen,ship):
